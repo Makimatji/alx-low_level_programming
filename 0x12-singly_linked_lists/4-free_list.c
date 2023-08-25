c7859c6ca31361b4b607cac302cb1b060f3310d7
Makimatji <dmmatji@gmail.com>
2023-08-25 06:24:46 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,9 +7,12 @@
  */
 void free_list(list_t *head)
 {
-    list_t* current = head;
-    while (current != NULL) {
-        list_t* temp = current;
+    list_t *current = head;
+
+    while (current != NULL)
+
+    {
+        list_t *temp = current;
         current = current->next;
         free(temp);
     }
